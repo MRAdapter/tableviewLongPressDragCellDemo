0eaa8c1ffa22ce898706b6bf17642f441a7c8fe2
wxt <2608920335@qq.com>
2016-07-13 12:01:49 +0800

    Initial Commit

---
+++ ADPDragTableVIewExample/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  ADPDragTableVIewExample
+//
+//  Created by wxt on 16/7/13.
+//  Copyright © 2016年 Adapter. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
